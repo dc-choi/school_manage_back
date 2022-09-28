2b112cd5edb987ab7decbb76d5c4cf437bd4d3bd
dc-choi <cheek0805@naver.com>
2022-09-28 10:27:49 +0900

    프로젝트 설정 추가
    1. DB 스키마 변경으로 인한 sequelize부분과 모델부분 변경
    2. 계정별로 로깅할 수 있도록 로그 파일 수정
    3. Server가 잘 구동할 수 있도록 서버 설정 변경

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,6 +1,5 @@
 import * as Sequelize from 'sequelize';
 import { DataTypes, Model, Optional } from 'sequelize';
-import type { Group, GroupId } from './group';
 
 export interface AccountAttributes {
     _id: number;
@@ -23,19 +22,6 @@ export class Account extends Model<AccountAttributes, AccountCreationAttributes>
     create_at!: Date;
     update_at?: Date;
     delete_at?: Date;
-
-    // Account hasMany Group via account__id
-    groups!: Group[];
-    getGroups!: Sequelize.HasManyGetAssociationsMixin<Group>;
-    setGroups!: Sequelize.HasManySetAssociationsMixin<Group, GroupId>;
-    addGroup!: Sequelize.HasManyAddAssociationMixin<Group, GroupId>;
-    addGroups!: Sequelize.HasManyAddAssociationsMixin<Group, GroupId>;
-    createGroup!: Sequelize.HasManyCreateAssociationMixin<Group>;
-    removeGroup!: Sequelize.HasManyRemoveAssociationMixin<Group, GroupId>;
-    removeGroups!: Sequelize.HasManyRemoveAssociationsMixin<Group, GroupId>;
-    hasGroup!: Sequelize.HasManyHasAssociationMixin<Group, GroupId>;
-    hasGroups!: Sequelize.HasManyHasAssociationsMixin<Group, GroupId>;
-    countGroups!: Sequelize.HasManyCountAssociationsMixin;
 
     static initModel(sequelize: Sequelize.Sequelize): typeof Account {
         return Account.init({
